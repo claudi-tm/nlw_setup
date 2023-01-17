561ef97412350e059e6b76f856bd8cbcb2e4fa8f
Cláudio Tomás <ccaducarlos@gmail.com>
2023-01-18 00:41:32 +0200

    made firsth route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log("Oina")
+import Fastify from "fastify";
+
+const app = Fastify()
+
+app.get("/", () => {
+    return "Hello NLW"
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log("Server HTTP Running"); 
+})
