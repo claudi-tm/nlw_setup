7a91c01ccf9892940a43070e315b138f9c1fd1fe
Cláudio Tomás <ccaducarlos@gmail.com>
2023-01-18 13:10:34 +0200

    ended setup of backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,27 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
-const app = Fastify()
+const app = Fastify();
+const client = new PrismaClient();
 
-app.get("/", () => {
-    return "Hello NLW"
+app.register(cors, {
+    
 })
+
+app.get("/", async () => {
+	const habits = await client.habit.create({
+		data: {
+			id: "",
+			created_at: "2023-02-01T00:00:00.000Z",
+			title: "comer saudavel",
+		},
+	});
+	return habits;
+});
 
 app.listen({
-    port: 3333,
+	port: 3333,
 }).then(() => {
-    console.log("Server HTTP Running"); 
-})
+	console.log("Server HTTP Running");
+});
