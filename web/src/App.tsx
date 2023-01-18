2a29d04823630d1573b0c878bb9e528aff206e93
Cláudio Tomás <ccaducarlos@gmail.com>
2023-01-18 14:23:59 +0200

    done with tailwindcss configuration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./styles/global.css" 
 import { Habit } from "./components/Habit";
 
+
 function App() {
-	const [count, setCount] = useState(0);
 
 	return(
     <div className="habit">
