2cc51bc50bb1a0736cb2be47d9a8e361922dfafb
Cláudio Tomás <ccaducarlos@gmail.com>
2023-01-21 07:48:26 +0200

    added Icon

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,19 @@
 import "./styles/global.css" 
 import { Habit } from "./components/Habit";
+import logoImage from "./assets/Group 2.svg"
 
 
 function App() {
 
 	return(
-    <div>
-      <Habit completed={3}></Habit>
-      <Habit completed={30}></Habit>
-      <Habit completed={300}></Habit>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <header>
+          <img src={logoImage} alt="Logo Image"/>
+        </header>
+      </div>
     </div>
-  ) 
+  )
 }
 
 export default App;
